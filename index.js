84126476a627f56b54d280dc739abae5abb09ba8
rarun9000 <rarun9000@gmail.com>
2022-09-07 00:28:30 +0530

    mes

--- index.js
+++ index.js
@@ -4,5 +4,7 @@ const app = express();
 
 const home = require('./routes/home');
 app.use('/', home);
-app.listen(3000);
+const port = process.env.PORT || 3001;
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
 console.log("Listening");
